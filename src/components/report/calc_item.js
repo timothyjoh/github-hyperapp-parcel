e06482dbec18ba81aee5b53d1c89c1b1ecdc1add
Tim Johnson <tjohnson@scitent.com>
2018-09-09 22:12:20 -0400

    refactor all the helper functions

---
+++ src/components/report/calc_item.js
@@ -0,0 +1,34 @@
+const getByVerb = verb => rs => rs.find( i => i.verb === verb )
+const correctAttempt = rs => rs.find(i => (i.context.correct === true))
+const gradedAttempt = rs => correctAttempt(rs) || getByVerb('second_attempt')(rs)
+
+export const iscorrect = rs => correctAttempt(rs) ? 1 : 0
+
+export const total_duration = rs => (
+  rs.reduce((t, r) => (t + r.context.timing.timer.total), 0)
+)
+
+export const duration_of = verb => rs => {
+  let r = getByVerb(verb)(rs)
+  return r ? r.context.timing.timer.total : 0
+}
+
+export const correctAnswerLetter = rs => {
+  const letters = "ABCDE".split('')
+  const ga = gradedAttempt(rs)
+  let result = {guess: '', correct: ''}
+  ga.context.item.responses.forEach((r,i) => {
+    if(r.displayText === ga.context.chosen_answer) { result.guess = letters[i] }
+    if(r.type[0] === 'correct') { result.correct = letters[i] }
+  })
+  return result
+}
+
+export const attempted = rs => rs.find(i => i.verb === 'first_attempt') ? 1 : 0
+export const completed = rs => skipped(rs) ? 0 : 1
+export const skipped = rs => {
+  let first = getByVerb('first_attempt')(rs)
+  let second = getByVerb('second_attempt')(rs)
+  let cor = iscorrect(rs)
+  return first && !cor && !second ? 1 : 0
+}
--- src/components/report/items.js
+++ src/components/report/items.js
@@ -1,37 +1,14 @@
 import { h } from 'hyperapp'
 import { sortByProp } from '../../lib/lib'
 import { groupByItemId } from './counts'
-
-const total_duration = rs => (
-  rs.reduce((t, r) => (t + r.context.timing.timer.total), 0)
-)
-const duration_of = verb => rs => {
-  let r = getByVerb(verb)(rs)
-  return r ? r.context.timing.timer.total : 0
-}
-const getByVerb = verb => rs => rs.find( i => i.verb === verb )
-const iscorrect = rs => correctAttempt(rs) ? 1 : 0
-const correctAttempt = rs => rs.find(i => (i.context.correct === true))
-const gradedAttempt = rs => correctAttempt(rs) || getByVerb('second_attempt')(rs)
-const correctAnswerLetter = rs => {
-  const letters = "ABCDE".split('')
-  const ga = gradedAttempt(rs)
-  let result = {guess: '', correct: ''}
-  ga.context.item.responses.forEach((r,i) => {
-    if(r.displayText === ga.context.chosen_answer) { result.guess = letters[i] }
-    if(r.type[0] === 'correct') { result.correct = letters[i] }
-  })
-  return result
-}
-
-const skipped = rs => {
-  let first = getByVerb('first_attempt')(rs)
-  let second = getByVerb('second_attempt')(rs)
-  let cor = iscorrect(rs)
-  return first && !cor && !second ? 1 : 0
-}
-const attempted = rs => rs.find(i => i.verb === 'first_attempt') ? 1 : 0
-const completed = rs => skipped(rs) ? 0 : 1
+import  { iscorrect,
+          total_duration,
+          duration_of,
+          attempted,
+          correctAnswerLetter,
+          completed,
+          skipped
+        } from './calc_item'
 
 // TODO: Need some extra data from the Sequence
 // which will give me items that were SEEN...
